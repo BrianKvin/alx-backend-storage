c6c6ba7ae3a5335c71121b8bd78a8abff73477b3
BrianKvin <briankvin@gmail.com>
2023-07-14 16:55:12 +0300

    Old school

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- lists all bands with Glam rock as main style, ranked by their longevity
+SELECT band_name
+IF (split, split - formed, 2022 - formed)
+	WHERE style = Glam rock
+       ORDER BY lifespan DESC;	
