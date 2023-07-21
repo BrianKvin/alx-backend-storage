b884b874f94415ccfa5890c391936b66819ee0c6
BrianKvin <briankvin@gmail.com>
2023-07-21 15:51:58 +0300

    Reading from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,20 +1,38 @@
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
+
+UnionOfTypes = Union[str, bytes, int, float]
+
 
 class Cache:
     '''
         Cache class.
     '''
-    def __init__(self):
+    def __init__(self):i
         '''
             Initialize the cache.
         '''
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: UnionOfTypes) -> str:
         ''' store data in the cache '''
         randomKey = str(uuid4())
         self._redis.set(randomKey, data)
         return randomKey
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> UnionOfTypes:
+        ''' get data from cache '''
+        if fn:
+            return fn(self._redis.get(key))
+        else:
+            return self._redis.get(key).decode()
+
+    def get_str(self: bytes) -> int:
+        """Returns a number"""
+        return int.from_bytes(self, sys.byteorder)
+
+    def get_int(self: bytes) -> str:
+        """ returns a string """
+        return self.decode("utf-8")
